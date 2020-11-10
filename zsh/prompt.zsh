a1543fc24cb961994e1e518837230abc53f800bd
Stepan Tubanov <stepan773@gmail.com>
2020-11-10 14:01:16 +0300

    Tab title, prompt color

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -8,8 +8,8 @@ git_dirty() {
 
 git_prompt_info () {
   ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
-  echo "%{\e[0m%} on %{\e[01m%}%{\e[38;5;105m%}%{%G%} ${ref#refs/heads/}$(git_dirty)"
+  echo "%{\e[0m%} on %{\e[01m%}%{\e[38;5;153m%}%{%G%} ${ref#refs/heads/}$(git_dirty)"
 }
 
 #export PROMPT=$'$FX[bold]$FG[075]%c $(git_prompt_info)$FG[105]%(!.#.»)$FX[reset]%{$reset_color%} '
-export PROMPT=$'%{\e[1;36m%}${PWD/#$HOME/~}$(git_prompt_info)%{\e[0m%} '
+export PROMPT=$'%{\e[01m%}%{\e[38;5;81m%}${PWD/#$HOME/~}$(git_prompt_info)%{\e[0m%} '
